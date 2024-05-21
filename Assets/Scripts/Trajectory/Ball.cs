c38482920821fd896041a8312c606d78954a3ed9
cinnamintRoll <96367858+cinnamintRoll@users.noreply.github.com>
2024-05-21 20:42:11 +0800

    Added Trajectory

---
+++ Assets/Scripts/Trajectory/Ball.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    private bool _isGhost;
+
+    public void Init(Vector3 velocity, bool isGhost)
+    {
+        _isGhost = isGhost;
+        _rb.AddForce(velocity, ForceMode.Impulse);
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (_isGhost) return;
+    }
+}
